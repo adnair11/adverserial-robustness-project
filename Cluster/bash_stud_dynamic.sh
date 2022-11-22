041ef87747b77f9ddc233fd9d18704c3fe84b3c3
LAPTOP-JUNAB2V2\sabretooth <adityanair.work@gmail.com>
2022-11-22 16:50:47 +0100

    new bash dynamic file

---
+++ Cluster/bash_stud_dynamic.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=dynamic_attacks
+#SBATCH --output=results/log/dynamic%j.log
+#SBATCH --error=results/err/dynamic%j.err
+#SBATCH --mail-user=coello@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+echo "Hello"
+        
+
+srun anaconda3/bin/python conda/SRP_cluster/main_stud_dynamic.py        # python jobs require the srun command to work
+echo "Running"
